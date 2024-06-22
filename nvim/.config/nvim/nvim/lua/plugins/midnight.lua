7dbf889d7d45bb24459a7e32bdbfbc6868589b2f
Leo <leo.reading@gmail.com>
2024-06-22 16:08:30 -0400

    Moving things around so I can use gnu stow

---
+++ nvim/.config/nvim/nvim/lua/plugins/midnight.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "lreading/midnight.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd([[colorscheme midnight]])
+      vim.cmd([[hi Normal guibg=NONE ctermbg=NONE]])
+    end
+  }
+}
+
