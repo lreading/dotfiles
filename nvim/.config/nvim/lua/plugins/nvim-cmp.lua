14bf79582868a74d4a864a2f4e989c37818e3470
Leo <leo.reading@gmail.com>
2024-07-08 15:14:24 -0400

    Removing snippets, not needed.  Updating lazy deps

--- nvim/.config/nvim/lua/plugins/nvim-cmp.lua
+++ nvim/.config/nvim/lua/plugins/nvim-cmp.lua
@@ -3,24 +3,11 @@ return {
     "hrsh7th/cmp-nvim-lsp",
   },
   {
-    "L3MON4D3/LuaSnip",
-    dependencies = {
-      "saadparwaiz1/cmp_luasnip",
-      "rafamadriz/friendly-snippets",
-    },
-  },
-  {
     "hrsh7th/nvim-cmp",
     config = function()
       local cmp = require("cmp")
-      require("luasnip.loaders.from_vscode").lazy_load()
 
       cmp.setup({
-        snippet = {
-          expand = function(args)
-            require("luasnip").lsp_expand(args.body)
-          end,
-        },
         window = {
           completion = cmp.config.window.bordered(),
           documentation = cmp.config.window.bordered(),
@@ -35,7 +22,6 @@ return {
         }),
         sources = cmp.config.sources({
           { name = "nvim_lsp" },
-          { name = "luasnip" }, -- For luasnip users.
         }, {
           { name = "buffer" },
         }),
--- nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,51 +1,36 @@
 return {
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-			local config = require("lspconfig")
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+      local config = require("lspconfig")
 
-			config.lua_ls.setup({
-				capabilities = capabilities,
-			})
-			config.html.setup({
-				capabilities = capabilities,
-			})
-			config.tsserver.setup({
-				capabilities = capabilities,
-			})
-			config.pyright.setup({
-				capabilities = capabilities,
-			})
-			config.eslint.setup({
-				capabilities = capabilities,
-				settings = {
-					packageManager = "pnpm",
-				},
-				on_attach = function(client)
-					vim.api.nvim_create_autocmd("BufWritePre", {
-						pattern = { "*.js", "*.jsx", "*.ts", "*.tsx", "*.vue" },
-						callback = function()
-              
-              vim.lsp.buf.execute_command({
-                command = "eslint.executeAutofix",
-                arguments = { vim.uri_from_bufnr(0) },
-              })
-							-- client.request("eslint/eslintAutofix", {
-							--	textDocument = vim.lsp.util.make_text_document_params(),
-							-- })
-						end,
-					})
-				end,
-			})
-			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.rename, {})
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-			vim.keymap.set("n", "<C-gk>", vim.lsp.buf.signature_help, {})
-			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-			vim.keymap.set("n", "<leader>f", function()
-				vim.lsp.buf.format({ async = true })
-			end, {})
-		end,
-	},
+      config.lua_ls.setup({
+        capabilities = capabilities,
+      })
+      config.html.setup({
+        capabilities = capabilities,
+      })
+      config.tsserver.setup({
+        capabilities = capabilities,
+      })
+      config.pyright.setup({
+        capabilities = capabilities,
+      })
+      config.eslint.setup({
+        capabilities = capabilities,
+        settings = {
+          packageManager = "pnpm",
+        },
+      })
+      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.rename, {})
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "<C-gk>", vim.lsp.buf.signature_help, {})
+      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+      vim.keymap.set("n", "<leader>f", function()
+        vim.lsp.buf.format({ async = true })
+      end, {})
+    end,
+  },
 }
