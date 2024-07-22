7437025fc31d9bc3a3807ac3af7ad6611cfb05bc
Leo <leo.reading@gmail.com>
2024-07-22 11:47:46 -0400

    Adding todo comments neovim plugin / configuration

---
+++ nvim/.config/nvim/lua/plugins/todo-comments.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {
+      alt = { "BUG", "LEO" },
+    },
+    config = function()
+      local comments = require("todo-comments")
+
+      vim.keymap.set("n", "<leader>td", ":TodoTelescope<CR>", { noremap = true, silent = true })
+
+      vim.keymap.set("n", "<leader>tn", function()
+        comments.jump_next()
+      end, { desc = "Next todo comment" })
+
+      vim.keymap.set("n", "<leader>tp", function()
+        comments.jump_prev()
+      end, { desc = "Previous todo comment" })
+
+      comments.setup()
+    end,
+  },
+}
