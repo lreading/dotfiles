dbaed80b4d21f90a113cd905064d8e8c56449228
Leo <leo.reading@gmail.com>
2024-09-01 11:48:41 -0400

    Nvim: Adding treesitter textobjects and updating plugins

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,6 +1,7 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
+    dependencies = { "nvim-treesitter/nvim-treesitter-textobjects" },
     build = ":TSUpdate",
     config = function()
       local config = require("nvim-treesitter.configs")
@@ -9,6 +10,37 @@ return {
         auto_install = true,
         highlight = { enable = true },
         indent = { enable = true },
+        textobjects = {
+          select = {
+            enable = true,
+            keymaps = {
+              ["af"] = "@function.outer",
+              ["if"] = "@function.inner",
+              ["ac"] = "@class.outer",
+              ["ic"] = "@class.inner",
+            },
+          },
+          swap = {
+            enable = true,
+            swap_next = { ["<leader>a"] = "@parameter.inner" },
+            swap_previous = { ["<leader>A"] = "@parameter.inner" },
+          },
+          move = {
+            enable = true,
+            set_jumps = true,
+            goto_next_start = { ["]m"] = "@function.outer" },
+            goto_next_end = { ["]M"] = "@function.outer" },
+            goto_previous_start = { ["[m"] = "@function.outer" },
+            goto_previous_end = { ["[M"] = "@function.outer" },
+          },
+          lsp_interop = {
+            enable = true,
+            peek_definition_code = {
+              ["gd"] = "@function.outer",
+              ["gD"] = "@class.outer",
+            },
+          },
+        },
       })
     end,
   },
