58a4bc3da0a24327932fa9b687ae9571d1e05dbc
Leo Reading <leo.reading@gmail.com>
2024-09-17 19:21:42 -0400

    Updating plugins, removing deprecated tsserver

--- nvim/.config/nvim/lua/plugins/mason-lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/mason-lspconfig.lua
@@ -1,12 +1,11 @@
 return {
   {
     "williamboman/mason-lspconfig.nvim",
-     config = function()
+    config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "tsserver", "pyright", "html", "eslint", "intelephense", "bashls", "jsonls", "yamlls", "dockerls" },
+        ensure_installed = { "lua_ls", "ts_ls", "pyright", "html", "eslint", "intelephense", "bashls", "jsonls", "yamlls", "dockerls" },
         automatic_installation = true,
       })
     end
   }
 }
-
