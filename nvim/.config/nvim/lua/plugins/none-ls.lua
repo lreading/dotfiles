31ef6e9f0fc901df5a43c5d03ee625d6c89cdf57
Leo Reading <leo.reading@gmail.com>
2024-09-17 20:00:41 -0400

    Upgrade stuff

--- nvim/.config/nvim/lua/plugins/none-ls.lua
+++ nvim/.config/nvim/lua/plugins/none-ls.lua
@@ -8,8 +8,10 @@ return {
       local null_ls = require("null-ls")
       null_ls.setup({
         sources = {
+          null_ls.builtins.completion.spell.with({
+            command = "aspell",
+          }),
           null_ls.builtins.formatting.stylua,
-          null_ls.builtins.completion.spell,
           null_ls.builtins.diagnostics.rubocop,
           null_ls.builtins.formatting.rubocop,
           null_ls.builtins.formatting.prettier.with({
