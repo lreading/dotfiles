351edd20bed7921a2ec58cef45bd839cf0ef4947
Leo <leo.reading@gmail.com>
2024-10-07 10:40:19 -0400

    Nvim tsserver debacle

--- nvim/.config/nvim/lua/plugins/mason-lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/mason-lspconfig.lua
@@ -3,9 +3,31 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "ts_ls", "pyright", "html", "eslint", "intelephense", "bashls", "jsonls", "yamlls", "dockerls" },
+        ensure_installed = {
+          "lua_ls",
+          "tsserver",
+          "pyright",
+          "html",
+          "eslint",
+          "intelephense",
+          "bashls",
+          "jsonls",
+          "yamlls",
+          "dockerls",
+        },
         automatic_installation = true,
       })
-    end
-  }
+      require("mason-lspconfig").setup({
+        function(server_name)
+          if server_name == "tsserver" then
+            server_name = "ts_ls"
+          end
+          local capabilities = require("cmp_nvim_lsp").default_capabilities()
+          require("lspconfig")[server_name].setup({
+            capabilities = capabilities,
+          })
+        end,
+      })
+    end,
+  },
 }
