eeb24a60221aa313d73ed9454ba15b6b1318d2d0
Leo Reading <leo.reading@gmail.com>
2025-02-08 10:53:21 -0500

    nvim: adding avante, updating telescope configuration to handle gitignores that use allow-lists

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -5,34 +5,56 @@ return {
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
       local builtin = require("telescope.builtin")
+
       require("telescope").setup({
         defaults = {
-          vimgrep_arguments = {
-            "rg",
-            "--color=never",
-            "--no-heading",
-            "--with-filename",
-            "--line-number",
-            "--column",
-            "--smart-case",
-            "--hidden",
-            "--glob",
-            "!.git/*",
-            "--glob",
-            "!node_modules/*",
-          },
           file_ignore_patterns = { "node_modules" },
-          find_command = { "rg", "--files", "--hidden", "--glob", "!.git/*" },
+          find_command = { "git", "ls-files", "--recurse-submodules" }, -- Only show Git-tracked files
+        },
+        pickers = {
+          find_files = {
+            find_command = { "git", "ls-files", "--recurse-submodules" },
+            cwd = vim.fn.getcwd(),
+          },
+          live_grep = {
+            cwd = vim.fn.getcwd(),
+            vimgrep_arguments = {
+              "rg",
+              "--color=never",
+              "--no-heading",
+              "--with-filename",
+              "--line-number",
+              "--column",
+              "--smart-case",
+              "--hidden",
+              "--no-ignore",
+              "--glob",
+              "!.git/*",
+              "--glob",
+              "!node_modules/*",
+              "--glob",
+              "!dist/*",
+            },
+          },
         },
       })
 
-      vim.keymap.set("n", "<C-p>", builtin.find_files, {})
+      -- Keymaps
+      vim.keymap.set("n", "<C-p>", function()
+        builtin.find_files({ cwd = vim.fn.getcwd() })
+      end, {})
+
       vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+
       vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
-      vim.keymap.set("n", "<leader>gg", builtin.git_files, {})
+      vim.keymap.set("n", "<leader>gg", function()
+        builtin.git_files({ cwd = vim.fn.getcwd() })
+      end, {})
+
       vim.keymap.set("n", "<leader>fs", function()
-        builtin.live_grep({ search_dirs = { "src", "tests", "*" } })
+        builtin.live_grep({ search_dirs = { "src", "tests", "*" }, cwd = vim.fn.getcwd() })
       end, {})
     end,
   },
 }
+
