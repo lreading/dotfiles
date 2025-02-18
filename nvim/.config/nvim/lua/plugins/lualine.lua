e5f7121919b3ae5d38d4261236b050c32e3dcff0
Leo Reading <leo.reading@gmail.com>
2025-02-18 10:54:03 -0500

    nvim: adding warning to lualine if there are unsaved buffers

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -2,10 +2,25 @@ return {
   {
     "nvim-lualine/lualine.nvim",
     config = function()
+      local function unsaved_buffers_indicator()
+        for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+          if vim.api.nvim_buf_get_option(buf, "modified") then
+            return "⚠️ Unsaved Buffers!"
+          end
+        end
+        return ""
+      end
+
       require("lualine").setup({
         options = {
-          theme = "dracula"
-        }
+          theme = "dracula",
+        },
+        sections = {
+          lualine_c = {
+            {"filename", path = 1, symbols = {modified = "[+]", readonly = "[-]"}},
+            {unsaved_buffers_indicator},
+          },
+        },
       })
     end
   }
