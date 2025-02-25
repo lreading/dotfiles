9e9267ffe7dc453abc7d2e263ec9eeab9a242688
Leo Reading <leo.reading@gmail.com>
2025-02-25 10:02:16 -0500

    nvim: updating keybindings for finding todos

--- nvim/.config/nvim/lua/plugins/todo-comments.lua
+++ nvim/.config/nvim/lua/plugins/todo-comments.lua
@@ -8,15 +8,15 @@ return {
     config = function()
       local comments = require("todo-comments")
 
-      vim.keymap.set("n", "<leader>td", ":TodoTelescope<CR>", { noremap = true, silent = true })
+      vim.keymap.set("n", "<leader>ft", ":TodoTelescope<CR>", { noremap = true, silent = true })
 
-      vim.keymap.set("n", "<leader>tn", function()
-        comments.jump_next()
-      end, { desc = "Next todo comment" })
+      -- vim.keymap.set("n", "<leader>tn", function()
+      --   comments.jump_next()
+      -- end, { desc = "Next todo comment" })
 
-      vim.keymap.set("n", "<leader>tp", function()
-        comments.jump_prev()
-      end, { desc = "Previous todo comment" })
+      -- vim.keymap.set("n", "<leader>tp", function()
+      --   comments.jump_prev()
+      -- end, { desc = "Previous todo comment" })
 
       comments.setup()
     end,
