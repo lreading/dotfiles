2a2453c83d540e6e75dbc7b3d386afc7190007f6
Leo Reading <leo.reading@gmail.com>
2025-12-11 21:20:17 -0500

    nvim: fixing neotree config

--- nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -9,11 +9,27 @@ return {
     },
     config = function()
       vim.keymap.set("n", "<leader>tr", ":Neotree filesystem reveal left toggle=true<CR>")
+
       require("neo-tree").setup({
         filesystem = {
+          -- make .git hidden but always show .github
+          filtered_items = {
+            visible = false,         -- start with hidden items actually hidden
+            show_hidden_count = true,
+            hide_dotfiles = true,
+            hide_gitignored = false,
+            hide_by_name = {
+              ".git",
+            },
+            always_show = {
+              ".github",
+            },
+          },
+
           window = {
             mappings = {
-              ["<cr>"] = "open",
+              ["<cr>"] = "conditional_enter",  -- 👈 our custom behavior
+              ["H"] = "toggle_hidden",         -- still keep a manual toggle
               ["t"] = "open_tabnew",
               ["d"] = "add_directory",
               ["D"] = "delete",
@@ -22,9 +38,28 @@ return {
               ["%"] = "add",
             },
           },
+
+          commands = {
+            conditional_enter = function(state)
+              local node = state.tree:get_node()
+              if not node then
+                return
+              end
+
+              -- The "(n hidden items)" line is a special "message" node.
+              -- For those, toggle hidden instead of trying to open.
+              if node.type == "message" then
+                require("neo-tree.sources.filesystem.commands").toggle_hidden(state)
+              else
+                require("neo-tree.sources.filesystem.commands").open(state)
+              end
+            end,
+          },
+
           hijack_netrw_behavior = "disabled",
           use_libuv_file_watcher = true,
         },
+
         auto_open = false,
         auto_close = true,
       })
@@ -39,6 +74,7 @@ return {
       end
 
       disable_auto_open()
-    end
-  }
+    end,
+  },
 }
+
