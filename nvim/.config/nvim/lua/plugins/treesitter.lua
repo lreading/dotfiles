ff1c4b9a155148e5cd8d0372ca8d1035a2225d25
Leo Reading <leo.reading@gmail.com>
2026-04-26 14:53:18 -0400

    nvim: updating treesitter to target new main branch

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,9 +1,15 @@
 return {
 	{
 		"nvim-treesitter/nvim-treesitter",
-		dependencies = { "nvim-treesitter/nvim-treesitter-textobjects" },
+		lazy = false,
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter-textobjects",
+		},
 		build = ":TSUpdate",
 		config = function()
+			local parsers = { "lua", "javascript", "vue", "rust", "markdown", "markdown_inline" }
+			local filetypes = { "lua", "javascript", "vue", "rust", "markdown" }
+
 			local function first_capture(match, capture_id)
 				local nodes = match[capture_id]
 				if type(nodes) == "table" then
@@ -63,44 +69,61 @@ return {
 				metadata[id].text = string.lower(text)
 			end, { force = true, all = false })
 
-			local config = require("nvim-treesitter.configs")
-			config.setup({
-				ensure_installed = { "lua", "javascript", "vue", "rust", "markdown", "markdown_inline" },
-				auto_install = true,
-				highlight = { enable = true },
-				indent = { enable = true },
-				textobjects = {
-					select = {
-						enable = true,
-						keymaps = {
-							["af"] = "@function.outer",
-							["if"] = "@function.inner",
-							["ac"] = "@class.outer",
-							["ic"] = "@class.inner",
-						},
-					},
-					swap = {
-						enable = true,
-						swap_next = { ["<leader>a"] = "@parameter.inner" },
-						swap_previous = { ["<leader>A"] = "@parameter.inner" },
-					},
-					move = {
-						enable = true,
-						set_jumps = true,
-						goto_next_start = { ["]m"] = "@function.outer" },
-						goto_next_end = { ["]M"] = "@function.outer" },
-						goto_previous_start = { ["[m"] = "@function.outer" },
-						goto_previous_end = { ["[M"] = "@function.outer" },
-					},
-					lsp_interop = {
-						enable = true,
-						peek_definition_code = {
-							["gd"] = "@function.outer",
-							["gD"] = "@class.outer",
-						},
-					},
+			local treesitter = require("nvim-treesitter")
+			treesitter.install(parsers)
+
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = filetypes,
+				callback = function()
+					vim.treesitter.start()
+					vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+				end,
+			})
+
+			require("nvim-treesitter-textobjects").setup({
+				select = {
+					lookahead = true,
+				},
+				move = {
+					set_jumps = true,
 				},
 			})
+
+			local select = require("nvim-treesitter-textobjects.select")
+			vim.keymap.set({ "x", "o" }, "af", function()
+				select.select_textobject("@function.outer", "textobjects")
+			end)
+			vim.keymap.set({ "x", "o" }, "if", function()
+				select.select_textobject("@function.inner", "textobjects")
+			end)
+			vim.keymap.set({ "x", "o" }, "ac", function()
+				select.select_textobject("@class.outer", "textobjects")
+			end)
+			vim.keymap.set({ "x", "o" }, "ic", function()
+				select.select_textobject("@class.inner", "textobjects")
+			end)
+
+			local swap = require("nvim-treesitter-textobjects.swap")
+			vim.keymap.set("n", "<leader>a", function()
+				swap.swap_next("@parameter.inner", "textobjects")
+			end)
+			vim.keymap.set("n", "<leader>A", function()
+				swap.swap_previous("@parameter.inner", "textobjects")
+			end)
+
+			local move = require("nvim-treesitter-textobjects.move")
+			vim.keymap.set({ "n", "x", "o" }, "]m", function()
+				move.goto_next_start("@function.outer", "textobjects")
+			end)
+			vim.keymap.set({ "n", "x", "o" }, "]M", function()
+				move.goto_next_end("@function.outer", "textobjects")
+			end)
+			vim.keymap.set({ "n", "x", "o" }, "[m", function()
+				move.goto_previous_start("@function.outer", "textobjects")
+			end)
+			vim.keymap.set({ "n", "x", "o" }, "[M", function()
+				move.goto_previous_end("@function.outer", "textobjects")
+			end)
 		end,
 	},
 }
