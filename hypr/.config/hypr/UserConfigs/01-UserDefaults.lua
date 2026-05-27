0a9bb0ffb43e1848109aee916467a9531884d07d
Leo Reading <leo.reading@gmail.com>
2026-05-26 21:53:02 -0400

    Revert "lua-fication of hyprland"
    This reverts commit 8955837d4f90ef5807ee52d129cb6e01480408da.

--- hypr/.config/hypr/UserConfigs/01-UserDefaults.lua
+++
@@ -1,7 +0,0 @@
--- Generated from 01-UserDefaults.conf. Edit the matching Lua file going forward.
-local H = require("lua.hypr_helpers")
-hl.env(H.expand("EDITOR"), H.expand("nvim"))
-H.set("edit", H.expand("${EDITOR:-nano}"))
-H.set("term", H.expand("kitty"))
-H.set("files", H.expand("thunar"))
-H.set("Search_Engine", H.expand("\"https://www.google.com/search?q={}\""))
--- hypr/.config/hypr/UserConfigs/LaptopDisplay.lua
+++
@@ -1,3 +0,0 @@
--- Generated from LaptopDisplay.conf. Edit the matching Lua file going forward.
-local H = require("lua.hypr_helpers")
-hl.monitor({ output = H.expand("eDP-1"), mode = H.expand("preferred"), position = H.expand("auto"), scale = 1.33 })
